ef0541c411848333146a1775a236d4b28e7f1367
Kaloyan Aleksiev <k.a.aleksiev@gmail.com>
2025-01-05 21:28:59 +0200

    Add Weight class

--- src/Weight.php
+++ src/Weight.php
@@ -2,6 +2,19 @@
 
 namespace KaloyanAleksiev\WeightConversions;
 
-class WeightConversionsClass
+class Weight
 {
+    public function __construct(protected float $kilometers)
+    {
+    }
+
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function toPounds(): float
+    {
+        return $this->kilometers * 2.2046;
+    }
 }
---
+++ tests/WeightTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use KaloyanAleksiev\WeightConversions\Weight;
+
+it('it can convert kilograms to pounds correctly', function () {
+    $pounds = Weight::kilograms(100)->toPounds();
+    expect($pounds)->toEqual(220.46);
+});
